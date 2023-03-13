55e38ff342d1998723a8556ca3c4531733808901
WTF <ivaneverovsky@mail.ru>
2023-03-13 14:27:26 +0300

    Добавьте файлы проекта.

---
+++ ResourceReport/Data/FileLoader.cs
@@ -0,0 +1,44 @@
+﻿using ExcelDataReader;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace ResourceReport.Data
+{
+    internal class FileLoader
+    {
+        public List<object> LoadFile(OpenFileDialog ofd)
+        {
+            List<object> fileData = new List<object>();
+
+            //using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            //{
+            //    using (var reader = ExcelReaderFactory.CreateReader(stream))
+            //    {
+            //        var result = reader.AsDataSet(); //get data from file
+
+            //        //fileData.Add(result);
+
+            //        for (int i = 0; i < result.Tables[0].Rows.Count; i++)
+            //        {
+            //            fileData.Add(result.Tables[0].Rows[i]);
+            //        }
+            //    }
+            //}
+
+            using (StreamReader sr = new StreamReader(ofd.FileName, Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                    fileData.Add(line);
+            }
+
+            //MessageBox.Show("Данные загружены.", "Готово");
+
+            return fileData;
+        }
+    }
+}
---
+++ ResourceReport/UI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using ResourceReport.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResourceReport
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileLoader _fl = new FileLoader();
+
+        List<object> fileRows = new List<object>(); //store data from file
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFile(object sender, RoutedEventArgs e) //load file
+        {
+            fileRows.Clear(); //clear file dictionary first
+
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Excel files (*.csv)|*.csv"
+            };
+            ofd.ShowDialog();
+
+            if (ofd.FileName == "")
+            {
+                MessageBox.Show("Файл не выбран.", "Внимание");
+                return;
+            }
+
+            fileRows = _fl.LoadFile(ofd);
+        }
+    }
+}
